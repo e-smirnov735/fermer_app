88abae2d69419407da384cd182870ffd7f6056b1
Evgeny Smirnov <e-smirnov@outlook.com>
2022-05-09 17:40:19 +0300

    add const app colors

---
+++ lib/src/core/styles/app_colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const primary = Color(0xFF0ACF83);
+  static const black = Colors.black;
+  static const white = Colors.white;
+  static const yellowAccent = Color(0xFFFFC120);
+  static const greyDark1 = Color(0xFF7F7F7F);
+  static const grey = Color(0xFFBABABA);
+  static const greyLight2 = Color(0xFFF3F3F3);
+  static const greyLight1 = Color(0xFFF6F6F6);
+}
